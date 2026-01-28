abba300e4fcb53c536c382754fc8e38a0eb3f7fe
Hema-ganesan <g.hema04.05.2006@gmail.com>
2026-01-28 14:12:45 +0530

    commit

--- src/wave.java
+++ src/wave.java
@@ -4,7 +4,7 @@ public class wave {
         int[] arr = {10, 5, 6, 3, 2, 20, 100, 80};
         int n = arr.length;
         for (int i = 0; i < n - 1; i++) {
-            if (i % 2 == 0) {   // even index
+            if (i % 2 == 0) {
                 if (arr[i] < arr[i + 1]) {
                     int temp = arr[i];
                     arr[i] = arr[i + 1];
@@ -15,8 +15,7 @@ public class wave {
                     int temp = arr[i];
                     arr[i] = arr[i + 1];
                     arr[i + 1] = temp;
-                }
-            }
+                }}
         }
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
