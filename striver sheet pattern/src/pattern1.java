e4f3ccc5b481d3b9dec0d883dc2c98502b65d0da
Hema-ganesan <g.hema04.05.2006@gmail.com>
2026-04-09 14:18:32 +0530

    programs

--- striver sheet pattern/src/pattern1.java
+++ striver sheet pattern/src/pattern1.java
@@ -4,6 +4,7 @@ public class pattern1 {
 //        * * * *
 //        * * * *
 //        * * * *
+
         for(int i=0;i<4;i++){
             for(int j=0;j<4;j++){
                 System.out.print("*");
