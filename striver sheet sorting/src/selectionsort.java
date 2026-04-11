7733fe7483bce9a098116514ada7e748883aa035
Hema-ganesan <g.hema04.05.2006@gmail.com>
2026-04-11 12:13:32 +0530

    programs

---
+++ striver sheet sorting/src/selectionsort.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class selectionsort {
+    public static void main(String[] args) {
+        int arr[]={10, 3, 2, 5, 7, 49, 30};
+//        for(int i=0;i<arr.length;i++) {
+//            int min = arr[i];
+//            int j;
+//            for (j = i + 1; j < arr.length-1; j++) {
+//                if (arr[j] < min) {
+//                    min = arr[j];
+//                }
+//            }
+//            int temp = min;
+//            min = arr[j];
+//            arr[j] = temp;
+//        }
+        for(int i=0;i<arr.length;i++){
+            int min=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]<arr[min]){
+                    min=j;
+                }
+            }
+            int temp=arr[i];
+            arr[i]=arr[min];
+            arr[min]=temp;
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
