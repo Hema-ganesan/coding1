f403dfcdfd39d321050a31ca5d9187860ad4e325
Hema-ganesan <g.hema04.05.2006@gmail.com>
2026-05-26 20:49:20 +0530

    programs

--- striver sheet linkedlist/src/singly.java
+++ striver sheet linkedlist/src/singly.java
@@ -13,17 +13,42 @@ public class singly {
     {
         int pos=2;
         int el=5;
+        int data=124;
         int[] arr={3,5,7,12};
         ListNode head=create(arr);
         System.out.println("AFTER CREATING LIST");
         printList(head);
         System.out.println();
+
+        //INSERTION
+        head=insertatfirst(head,2);
+        System.out.println("AFTER INSERTING AT FIRST ELEMENT");
+        printList(head);
+        System.out.println();
+        head=insertatlast(head,15);
+        System.out.println("AFTER INSERTING AT LAST ELEMENT");
+        printList(head);
+        System.out.println();
+        head=insertatpos(head,data,pos);
+        System.out.println("AFTER INSERTING AT POS");
+        printList(head);
+        System.out.println();
+        head=insertbeforeval(head,121,5);
+        System.out.println("INSERTING BEFORE A VALUE");
+        printList(head);
+        System.out.println();
+        head=insertafterval(head,6,5);
+        System.out.println("INSERTING AFTER A VALUE");
+        printList(head);
+        System.out.println();
+
+        //DELETION
         head=deleteatfirst(head);
-        System.out.println("AFTER DELETING FIRST ELEMENT");
+        System.out.println("AFTER DELETING AT FIRST ELEMENT");
         printList(head);
         System.out.println();
         head=deleteatlast(head);
-        System.out.println("AFTER DELETING LAST ELEMENT");
+        System.out.println("AFTER DELETING AT LAST ELEMENT");
         printList(head);
         System.out.println();
         head=deleteatpos(head,pos);
@@ -35,6 +60,15 @@ public class singly {
         printList(head);
         System.out.println();
 
+        //LENGTH
+        System.out.println("LENGTH OF THE LIST");
+        System.out.println(findlength(head));
+        System.out.println();
+
+        //ELEMENT EXIST OR NOT
+        System.out.println("ELEMENT EXIST OR NOT");
+        System.out.println(elementexistornot(head,12));
+        System.out.println();
     }
 
 
@@ -64,6 +98,84 @@ public class singly {
         }
         return head;
     }
+    //INSERTION FUNCTION
+    public static ListNode insertatfirst(ListNode head,int val)
+    {
+        ListNode temp=new ListNode(val,head);
+        temp.next=head;
+        head=temp;
+        return head;
+    }
+
+    public static ListNode insertatlast(ListNode head,int val)
+    {
+        ListNode temp=head;
+        while(temp.next!=null)
+        {
+            temp=temp.next;
+        }
+        if(temp.next==null){
+            temp.next=new ListNode(val,null);
+        }
+        return head;
+    }
+
+    public static ListNode insertatpos(ListNode head,int data,int pos)
+    {
+        ListNode temp=head;
+        ListNode temp1;
+        int i=0;
+        if(pos==0){
+            head=new ListNode(data,null);
+            if(temp!=null)
+            {
+                head.next=temp;
+            }
+        }
+        while(temp.next!=null){
+            i++;
+            temp=temp.next;
+            if(i==pos-1){
+                temp1=new ListNode(data,null);
+                temp1.next=temp.next;
+                temp.next=temp1;
+            }
+        }
+        return head;
+    }
+
+    public static ListNode insertbeforeval(ListNode head,int val,int el)
+    {
+        ListNode temp=head;
+        ListNode temp1;
+        while(temp.next!=null){
+            if(temp.next.val==el){
+                temp1=new ListNode(val,null);
+                temp1.next=temp.next;
+                temp.next=temp1;
+                break;
+            }
+            temp=temp.next;
+        }
+        return head;
+    }
+
+    public static ListNode insertafterval(ListNode head,int val,int el)
+    {
+        ListNode temp=head;
+        ListNode temp1;
+        while(temp.next!=null){
+            if(temp.val==el){
+                temp1=new ListNode(val,null);
+                temp1.next=temp.next;
+                temp.next=temp1;
+                break;
+            }
+            temp=temp.next;
+        }
+        return head;
+    }
+    //DELETION FUNCTION
     public static ListNode deleteatfirst(ListNode head)
     {
         if(head==null){
@@ -71,6 +183,7 @@ public class singly {
         }
         return head.next;
     }
+
     private static ListNode deleteatlast(ListNode head) {
         ListNode temp=head;
         while(temp.next.next!=null){
@@ -99,6 +212,7 @@ public class singly {
         }
         return head;
     }
+
     public static ListNode deletebyelement(ListNode head,int el){
         ListNode temp=head;
         if(el==head.val){
@@ -107,7 +221,7 @@ public class singly {
         if(head==null){
             return null;
         }
-        while(temp!=null){
+        while(temp.next!=null){
             if(temp.next.val==el){
                 temp.next=temp.next.next;
                 break;
@@ -115,5 +229,30 @@ public class singly {
             temp=temp.next;
         }
         return head;
+    }
+
+    //LENGTH
+    public static int findlength(ListNode head)
+    {
+        int cnt=0;
+        ListNode temp=head;
+        while(temp!=null){
+            cnt++;
+            temp=temp.next;
+        }
+        return cnt;
+    }
+
+    //ELEMENT EXIST OR NOT
+    public static Boolean elementexistornot(ListNode head,int val)
+    {
+        ListNode temp=head;
+        while(temp!=null){
+            if(temp.val==val){
+                return true;
+            }
+            temp=temp.next;
+        }
+        return false;
     }
 }
