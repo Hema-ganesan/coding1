2bcf8d628617d75c3e5c886aa7296d0ae43b9f75
Hema-ganesan <g.hema04.05.2006@gmail.com>
2026-07-09 23:18:38 +0530

    programs

---
+++ striver sheet array/src/rightrotatearraybydplace.java
@@ -0,0 +1,23 @@
+public class rightrotatearraybydplace {
+    public static void main(String[] args) {
+        int[] arr={1,3,4,2,5,7};
+        int d=3;
+        d=d%arr.length;
+        reverse(arr,0,arr.length-1);
+        reverse(arr,0,d-1);
+        reverse(arr,d,arr.length-1);
+
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void reverse(int[] n,int st,int end){
+        while(st<end){
+            int temp=n[st];
+            n[st]=n[end];
+            n[end]=temp;
+            st++;
+            end--;
+        }
+    }
+}
