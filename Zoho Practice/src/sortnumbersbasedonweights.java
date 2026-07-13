9edcc4a6791b3f1d8d0b5bae0bb3e3b0fa530f73
Hema-ganesan <g.hema04.05.2006@gmail.com>
2026-07-13 18:15:09 +0530

    programs

--- Zoho Practice/src/sortnumbersbasedonweights.java
+++ Zoho Practice/src/sortnumbersbasedonweights.java
@@ -1,3 +1,35 @@
+//PROBLEM STATEMENT:
+//Explanation:
+//To calculate the weight the rules are:
+//1. if a number is  even number add 3
+//2. if a number is a multiple of 6 and divisible of 4 add 4
+//3. if a number is a perfect square add 5
+//
+//let us take example if this is my input
+//Input:
+//enter number of values:n
+//n=5
+//10
+//36
+//49
+//53
+//12
+//
+//The output should be in the descending order of their weight
+//Output:
+//36
+//12
+//49
+//10
+//28
+//The Output should depend on
+//36 -> 12
+//12 -> 7
+//49 -> 5
+//10 -> 3
+//53 -> 0
+
+
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -45,7 +77,7 @@ public class sortnumbersbasedonweights {
         }
         System.out.println();
         for (int i = 0; i < n; i++) {
-            System.out.println(arr[i]);// + " -> " + res[i]);
+            System.out.println(arr[i] + " -> " + res[i]);
         }
     }
 }
